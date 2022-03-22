a5741b30a45d102cb3cf50e300d87ffe0241666a
Andy Yglecias <andyyglecias@gmail.com>
2022-03-22 12:38:34 -0500

    changed number of votes to number of requests

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 /* global Chart, axios */
 
+let tally = [];
+let labels = [];
+for (let index = 0; index <= 50; index++) {
+  labels.push(`Ward ${index}`);
+}
+
 axios
   .get("https://data.cityofchicago.org/resource/hec5-y4x5.json")
   .then(function (response) {
     // handle success
     //console.log(response.data);
-    let tally = {};
     for (let removalRequest of response.data) {
       let wardNumber = removalRequest.ward;
       if (tally[wardNumber]) {
@@ -14,6 +19,7 @@ axios
         tally[wardNumber] = 1;
       }
     }
+    console.log(tally);
   })
   .catch(function (error) {
     // handle error
@@ -27,27 +33,27 @@ const ctx = document.getElementById("myChart").getContext("2d");
 const myChart = new Chart(ctx, {
   type: "bar",
   data: {
-    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    labels: labels,
     datasets: [
       {
-        label: "# of Votes",
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: [
-          "rgba(255, 99, 132, 0.2)",
-          "rgba(54, 162, 235, 0.2)",
-          "rgba(255, 206, 86, 0.2)",
-          "rgba(75, 192, 192, 0.2)",
-          "rgba(153, 102, 255, 0.2)",
-          "rgba(255, 159, 64, 0.2)",
-        ],
-        borderColor: [
-          "rgba(255, 99, 132, 1)",
-          "rgba(54, 162, 235, 1)",
-          "rgba(255, 206, 86, 1)",
-          "rgba(75, 192, 192, 1)",
-          "rgba(153, 102, 255, 1)",
-          "rgba(255, 159, 64, 1)",
-        ],
+        label: "# of Requests",
+        data: tally,
+        // backgroundColor: [
+        //   "rgba(255, 99, 132, 0.2)",
+        //   "rgba(54, 162, 235, 0.2)",
+        //   "rgba(255, 206, 86, 0.2)",
+        //   "rgba(75, 192, 192, 0.2)",
+        //   "rgba(153, 102, 255, 0.2)",
+        //   "rgba(255, 159, 64, 0.2)",
+        // ],
+        // borderColor: [
+        //   "rgba(255, 99, 132, 1)",
+        //   "rgba(54, 162, 235, 1)",
+        //   "rgba(255, 206, 86, 1)",
+        //   "rgba(75, 192, 192, 1)",
+        //   "rgba(153, 102, 255, 1)",
+        //   "rgba(255, 159, 64, 1)",
+        // ],
         borderWidth: 1,
       },
     ],
